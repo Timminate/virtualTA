1a0a01ab358bc8e5b75df38afec73926552e9d1f
shagundalal <dalal.shagun@gmail.com>
2024-04-01 14:43:55 -0500

    Profile page basic

--- front-end4/src/app/page.tsx
+++ front-end4/src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/profile"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
